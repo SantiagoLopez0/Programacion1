112a327ca63670100f9a34d3ffd1f31fb53cae1a
SantiagoLopez0 <asantiago0034@gmail.com>
2019-10-20 23:21:13 -0500

    Desarrollo de backend y conexion a DB de usuarios y login. cambios de estilo de home

--- Proyecto Seminario/assets/js/login.js
+++ Proyecto Seminario/assets/js/login.js
@@ -1,3 +1,43 @@
 $(function(){
-  $('.title1').show('puff', 1500);
-});
+  var l = new Login();
+})
+
+
+class Login {
+  constructor() {
+    this.submitEvent()
+  }
+
+  submitEvent(){
+    $('form').submit((event)=>{
+      event.preventDefault()
+      this.sendForm()
+    })
+  }
+
+  sendForm(){
+    let form_data = new FormData();
+    form_data.append('username', $('#user').val())
+    form_data.append('password', $('#password').val())
+    $.ajax({
+      url: '../Proyecto Seminario/server/login.php',
+      dataType: "json",
+      cache: false,
+      processData: false,
+      contentType: false,
+      data: form_data,
+      type: 'POST',
+      success: function(php_response){
+        if (php_response.msg == "OK") {
+          window.location.href = 'index.html';
+        }else {
+          alert(php_response.msg);
+          alert(php_response.motivo);
+        }
+      },
+      error: function(err){
+        console.log(err);
+      }
+    });
+  }
+}
